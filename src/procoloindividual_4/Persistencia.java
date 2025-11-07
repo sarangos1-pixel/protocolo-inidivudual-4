469d66462c4c1c9da73179341bca711218321f86
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 18:03:56 -0500

    clase libro persistencia ejercicio 1

---
+++ src/procoloindividual_4/Persistencia.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package procoloindividual_4;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Persistencia {
+    public void GuardarDateBase(Libro libro) {
+        System.out.println("|Guardando libro en la base de datos: " + libro.getTitulo());
+    }
+}
