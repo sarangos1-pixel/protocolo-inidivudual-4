a265fcde6fbbb5272d370779a0b77ed5c03db2ac
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 17:57:44 -0500

    clase libro generarEtiqueta ejercicio 1

---
+++ src/procoloindividual_4/GenerarEtiqueta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package procoloindividual_4;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class GenerarEtiqueta {
+    public void GenerarEtiqueta(Producto producto) {
+        System.out.println("Etiqueta: " + producto.getNombre() + " - $" + producto.getPrecio());
+    }
+
+}
